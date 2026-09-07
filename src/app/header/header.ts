5739167fe7424a8124e662d9c64b640a516ec959
Vitor Gabriel Nascimento França <vitorgn1franca@protonmail.com>
2026-09-07 14:17:30 -0300

    feat: added nav bar

--- src/app/header/header.ts
+++ src/app/header/header.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   selector: 'app-header',
   styleUrl: './header.css',
   templateUrl: './header.html',
