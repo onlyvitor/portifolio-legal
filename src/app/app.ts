59262d84e5793c73a3ce299c76db1a0ce068e8cd
Vitor Gabriel Nascimento França <vitorgn1franca@protonmail.com>
2026-09-10 22:54:52 -0300

    feat: import Home component in App

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './header/header';
+import { Home } from './home/home';
 
 @Component({
-  imports: [RouterOutlet, Header],
+  imports: [RouterOutlet, Header, Home],
   selector: 'app-root',
   styleUrl: './app.css',
   templateUrl: './app.html',
