036d583b14ebf0b681a78ddb8d726e87148cc9cf
Vitor Gabriel Nascimento França <vitorgn1franca@protonmail.com>
2026-09-10 22:55:40 -0300

    fix: remove unused Home import from App

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,9 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './header/header';
-import { Home } from './home/home';
 
 @Component({
-  imports: [RouterOutlet, Header, Home],
+  imports: [RouterOutlet, Header],
   selector: 'app-root',
   styleUrl: './app.css',
   templateUrl: './app.html',
